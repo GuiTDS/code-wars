d62872a92b12ce82a8806c153374d4b24692149b
Guilherme Sanches <guitdsanches@gmail.com>
2023-04-17 21:05:45 -0300

    highAndLow

---
+++ highAndLow.js
@@ -0,0 +1,9 @@
+function highAndLow(numbers){
+    numbers = numbers.split(' ');
+    return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+
+let result = highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
+console.log(result);
+
+
